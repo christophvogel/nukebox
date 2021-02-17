1bfb7d2ff67d0b2b50b282669b2c90d8dc549999
christophvogel <christoph@boerd.net>
2021-02-17 10:16:57 +0100

    ✨ Add new Trackitems

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,14 +19,14 @@ export default function Home() {
         artist="Procol Harum"
       />
       <TracklistItem
-        imgSrc="https://note-store.de/upload/resize_cache/iblock/baa/340_420_0/Procol-Harum-_-A-Whiter-Shade-of-Pale.jpg"
-        title="A Whiter Shade of Pale"
-        artist="Procol Harum"
+        imgSrc="https://img.discogs.com/MW6n8H8M4Hyu3EFVfKG5Eu9sRng=/fit-in/599x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-918683-1174128663.jpeg.jpg"
+        title="Last Nite"
+        artist="The Strokes"
       />
       <TracklistItem
-        imgSrc="https://note-store.de/upload/resize_cache/iblock/baa/340_420_0/Procol-Harum-_-A-Whiter-Shade-of-Pale.jpg"
-        title="A Whiter Shade of Pale"
-        artist="Procol Harum"
+        imgSrc="https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/The_Black_Keys_-_Tighten_Up.jpg/220px-The_Black_Keys_-_Tighten_Up.jpg"
+        title="Tighten Up"
+        artist="The Black Keys"
       />
     </div>
   );
