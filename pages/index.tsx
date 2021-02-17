f0811d0b2f2c07d9e77570bc0e5b9f52e387bee8
christophvogel <christoph@boerd.net>
2021-02-17 09:59:00 +0100

    💄 Add css

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,16 @@ export default function Home() {
         title="A Whiter Shade of Pale"
         artist="Procol Harum"
       />
+      <TracklistItem
+        imgSrc="https://note-store.de/upload/resize_cache/iblock/baa/340_420_0/Procol-Harum-_-A-Whiter-Shade-of-Pale.jpg"
+        title="A Whiter Shade of Pale"
+        artist="Procol Harum"
+      />
+      <TracklistItem
+        imgSrc="https://note-store.de/upload/resize_cache/iblock/baa/340_420_0/Procol-Harum-_-A-Whiter-Shade-of-Pale.jpg"
+        title="A Whiter Shade of Pale"
+        artist="Procol Harum"
+      />
     </div>
   );
 }
