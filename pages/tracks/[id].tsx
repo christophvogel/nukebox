6df30d4f077f7a03d66240461b881072119665a6
christophvogel <christoph@boerd.net>
2021-02-24 17:19:47 +0100

    ✨ Create fetchURL helper function

--- pages/tracks/[id].tsx
+++ pages/tracks/[id].tsx
@@ -10,7 +10,10 @@ import styles from "../../styles/Favbutton.module.css";
 export default function Track() {
   const router = useRouter();
   const { id: idQuery } = router.query;
-  const id = typeof idQuery === "string" ? idQuery : idQuery[0];
+  if (!idQuery) {
+    return null;
+  }
+  const id = typeof idQuery !== "string" ? idQuery[0] : idQuery;
   const [track, setTrack] = useState<APITrack>(null);
   const [favoriteSongs, setFavoriteSongs] = useLocalStorage<string[]>(
     "favoriteSongs",
